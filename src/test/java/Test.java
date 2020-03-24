0c335b99d617fd1fbc224f65aa4e6a83fc8ac0b0
Ku3ina <62447555+Ku3ina@users.noreply.github.com>
2020-03-24 21:33:30 +0300

    test
    some staff

---
+++ src/test/java/Test.java
@@ -0,0 +1,30 @@
+import HW3.Calculation;
+
+/**
+ * Код писать здесь
+ */
+// работаю в файле от 22.03.2020 20:42
+
+public class Test {
+    public static void main(String[] args) {
+//        System.out.println("при значении зп ");
+
+
+
+      /* double a = 12.1;
+       Calculation.discount(a);
+       System.out.println("(при значении salary " + a + ")");
+       double b = 6;
+       Calculation.discount(a);
+       System.out.println("(при значении salary " + b + ")");
+      //  Calculation.discount(14.0);
+      //  Calculation.discount(-0.0003);
+      //  Calculation.discount(2.4);
+        int ticketPrice = 400;
+        double discount = 13.33333;
+        System.out.println();
+        System.out.println("цена на билет " + Calculation.countTicketPrice(ticketPrice, discount));
+        System.out.println("(при значениях цены " + ticketPrice + " и скидки " + discount + ")");
+  */  }
+
+}
